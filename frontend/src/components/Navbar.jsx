7426422937f26053b32f5b4df86172c912582310
maria shareef <mariashareef2015@gmail.com>
2025-07-03 18:33:43 +0300

    Fix rollup optional dependency issue

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -69,7 +69,7 @@ const Navbar = () => {
           {token && (
             <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
               <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-50 text-[#fdb557] rounded">
-                <p className="cursor-pointer hover:text-black">My Profile</p>
+                {/* <p className="cursor-pointer hover:text-black">My Profile</p> */}
 
                 <p
                   onClick={() => navigate("/orders")}
